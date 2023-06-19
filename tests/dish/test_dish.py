65c480531932dc28d2b546c55917baeac7cdd512
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-19 16:18:43 -0300

    Feat: Testa método mágico __eq__

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -3,9 +3,14 @@ from src.models.dish import Dish  # noqa: F401, E261, E501
 
 def test_dish():
     dish1 = Dish("Macarrão com camarão", 32.00)
+    dish2 = Dish("Omelete de queijo", 18.00)
 
     assert dish1.name == "Macarrão com camarão"
     assert dish1.price == 32.00
 
     assert repr(dish1) == "Dish('Macarrão com camarão', R$32.00)"
     # return f"Dish('{self.name}', R${self.price:.2f})"
+
+    assert dish1 != dish2
+    assert dish2 == Dish("Omelete de queijo", 18.00)
+    # return self.__repr__() == other.__repr__()
