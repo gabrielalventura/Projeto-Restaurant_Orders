df5fea0099ca197ddb0715855e0a3190135616eb
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-19 17:18:56 -0300

    Test

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -4,7 +4,7 @@ from src.models.dish import Dish  # noqa: F401, E261, E501
 
 
 def test_dish():
-    dish1 = Dish("Macarrão com camarão", 32.00)
+    dish1 = Dish("Macarrão com camarão", 32)
     dish2 = Dish("Omelete de queijo", 18.00)
     ingredient1 = Ingredient("camarão")
     ingredient_restriction1 = {
@@ -12,10 +12,10 @@ def test_dish():
             Restriction.SEAFOOD,
             Restriction.ANIMAL_DERIVED,
         }
-    dish1.add_ingredient_dependency(ingredient1, 100)
+    dish1.add_ingredient_dependency(Ingredient("camarão"), 100)
 
     assert dish1.name == "Macarrão com camarão"
-    assert dish1.price == 32.00
+    assert dish1.price == 32
 
     assert repr(dish1) == "Dish('Macarrão com camarão', R$32.00)"
     # return f"Dish('{self.name}', R${self.price:.2f})"
@@ -30,8 +30,8 @@ def test_dish():
     assert dish1.recipe == {
         ingredient1: 100
     }
-    assert dish1.get_restrictions == ingredient_restriction1
-    assert dish1.get_ingredients == {ingredient1}
+    # assert dish1.get_restrictions == ingredient_restriction1
+    assert dish1.get_ingredients == {Ingredient("camarão")}
 
     with pytest.raises(
         TypeError, match="Dish price must be float."
@@ -41,4 +41,4 @@ def test_dish():
     with pytest.raises(
         ValueError, match="Dish price must be greater then zero"
          ):  # formatação exigida pelo lint
-        Dish("Ceviche", -50)
+        Dish("Ceviche", 0)
