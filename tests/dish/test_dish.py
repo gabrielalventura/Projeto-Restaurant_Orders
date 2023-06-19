9b5607924d0aaa1126eee04e685a85dd8eafebbb
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-19 16:28:30 -0300

    Feat: retorno correto de Recipes após a adição de ingredientes

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,9 +1,12 @@
+from models.ingredient import Ingredient
 from src.models.dish import Dish  # noqa: F401, E261, E501
 
 
 def test_dish():
     dish1 = Dish("Macarrão com camarão", 32.00)
     dish2 = Dish("Omelete de queijo", 18.00)
+    ingredient1 = Ingredient("camarão")
+    dish1.add_ingredient_dependency(ingredient1, 100)
 
     assert dish1.name == "Macarrão com camarão"
     assert dish1.price == 32.00
@@ -17,3 +20,7 @@ def test_dish():
 
     assert hash(dish1) == "Dish('Macarrão com camarão', R$32.00)"
     # return hash(self.__repr__())
+
+    assert dish1.recipe == {
+        ingredient1: 100
+    }
