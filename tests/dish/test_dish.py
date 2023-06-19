baa8ad5b09341c1a2e694578c111f570472ef0b5
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-19 17:27:25 -0300

    Fix: chama os metodos get corretamente e testa TypeError com string

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -4,18 +4,18 @@ from src.models.dish import Dish  # noqa: F401, E261, E501
 
 
 def test_dish():
-    dish1 = Dish("Macarrão com camarão", 32)
+    dish1 = Dish("Macarrão com camarão", 32.00)
     dish2 = Dish("Omelete de queijo", 18.00)
-    ingredient1 = Ingredient("camarão")
+    ingredient1 = Ingredient('camarão')
     ingredient_restriction1 = {
             Restriction.ANIMAL_MEAT,
             Restriction.SEAFOOD,
             Restriction.ANIMAL_DERIVED,
         }
-    dish1.add_ingredient_dependency(Ingredient("camarão"), 100)
+    dish1.add_ingredient_dependency(ingredient1, 100)
 
     assert dish1.name == "Macarrão com camarão"
-    assert dish1.price == 32
+    assert dish1.price == 32.00
 
     assert repr(dish1) == "Dish('Macarrão com camarão', R$32.00)"
     # return f"Dish('{self.name}', R${self.price:.2f})"
@@ -30,13 +30,13 @@ def test_dish():
     assert dish1.recipe == {
         ingredient1: 100
     }
-    # assert dish1.get_restrictions == ingredient_restriction1
-    assert dish1.get_ingredients == {Ingredient("camarão")}
+    assert dish1.get_restrictions() == ingredient_restriction1
+    assert dish1.get_ingredients() == {ingredient1}
 
     with pytest.raises(
         TypeError, match="Dish price must be float."
          ):  # formatação exigida pelo lint
-        Dish("Ceviche", -50)
+        Dish("Ceviche", "50")
 
     with pytest.raises(
         ValueError, match="Dish price must be greater then zero"
