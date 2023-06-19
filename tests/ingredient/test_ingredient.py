7970706730e91940bdf337a59d78f109c43ab2b1
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-19 15:12:27 -0300

    Feat: Testa se o método mágico __eq__ funciona como esperado

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -2,13 +2,18 @@ from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E
 
 
 def test_ingredient():
-    ingredient = Ingredient("camarão")
-    ingredient_restriction = {
+    ingredient1 = Ingredient("camarão")
+    ingredient2 = Ingredient("caldo de carne")
+
+    ingredient_restriction1 = {
             Restriction.ANIMAL_MEAT,
             Restriction.SEAFOOD,
             Restriction.ANIMAL_DERIVED,
         }
-    assert ingredient.name == "camarão"
-    assert ingredient.restrictions == ingredient_restriction
+    assert ingredient1.name == "camarão"
+    assert ingredient1.restrictions == ingredient_restriction1
 
-    assert repr(ingredient) == "Ingredient('camarão')"
+    assert repr(ingredient1) == "Ingredient('camarão')"
+
+    assert ingredient1 != ingredient2
+    # return repr(self) == repr(other)
